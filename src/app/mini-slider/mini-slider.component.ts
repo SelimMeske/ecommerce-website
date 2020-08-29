d9c5d6e102395b4ecca4457aea19c5888913a420
SelimMeske <mesic.selim@outlook.com>
2020-08-29 19:04:56 +0200

    added slider arrows

--- src/app/mini-slider/mini-slider.component.ts
+++ src/app/mini-slider/mini-slider.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ImageObject } from '../interfaces/image-object';
 
 @Component({
   selector: 'app-mini-slider',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MiniSliderComponent implements OnInit {
 
+  images: ImageObject[] = [
+    {altText: 'mini-slider-image', source: 'assets/products for mini-slider/pr1.jpg'},
+    {altText: 'mini-slider-image', source: 'assets/products for mini-slider/pr2.jpg'},
+    {altText: 'mini-slider-image', source: 'assets/products for mini-slider/pr3.jpg'},
+    {altText: 'mini-slider-image', source: 'assets/products for mini-slider/pr4.jpg'},
+    {altText: 'mini-slider-image', source: 'assets/products for mini-slider/pr5.jpg'},
+    {altText: 'mini-slider-image', source: 'assets/products for mini-slider/pr6.jpg'},
+    {altText: 'mini-slider-image', source: 'assets/products for mini-slider/pr7.jpg'},
+    {altText: 'mini-slider-image', source: 'assets/products for mini-slider/pr8.jpg'},
+    {altText: 'mini-slider-image', source: 'assets/products for mini-slider/pr8.jpg'}
+
+    ]
+
   constructor() { }
 
   ngOnInit(): void {
+    //set slider width
+    let slider = document.querySelector('.slider-wrap');
+    (slider as HTMLDivElement).style.width = this.images.length * 287.5 + 'px';
   }
 
 }
