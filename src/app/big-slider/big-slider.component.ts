c04987a923413d23781e1ec795860a630dcde3dd
SelimMeske <mesic.selim@outlook.com>
2020-09-04 18:17:30 +0200

    slider update

--- src/app/big-slider/big-slider.component.ts
+++ src/app/big-slider/big-slider.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductObject } from '../interfaces/product-object';
 
 @Component({
   selector: 'app-big-slider',
@@ -17,22 +16,32 @@ export class BigSliderComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  
+
+    let nextButton = document.querySelector('.bs-next');
+    let prevButton = document.querySelector('.bs-prev');
+    let imageElements = document.querySelectorAll('.slider-image');
+    let dotElements = document.querySelectorAll('.dot');
+
     setTimeout(() => {
-      let imagesElements = document.querySelectorAll('.slider-image');
       
+      
+
       setInterval(() => {
-        for(let i = 0; i < imagesElements.length; i++) {
-          if(imagesElements[i].classList.contains('active-image')){
-            if(i === imagesElements.length-1){
-              (imagesElements[i] as HTMLDivElement).classList.add('zindex-image');
-              imagesElements[0].classList.add('active-image');
-              this.removeActiveClass((imagesElements[i] as HTMLDivElement))
+        for(let i = 0; i < imageElements.length; i++) {
+          if(imageElements[i].classList.contains('active-image')){
+            if(i === imageElements.length-1){
+              (imageElements[i] as HTMLDivElement).classList.add('zindex-image');
+              imageElements[0].classList.add('active-image');
+              dotElements[0].classList.add('active-dot');
+              this.removeActiveClass((imageElements[i] as HTMLDivElement));
+              this.removeDotActiveClass((dotElements[i] as HTMLDivElement));
               break;
             }else {
-              (imagesElements[i] as HTMLDivElement).classList.add('zindex-image');
-              imagesElements[i+1].classList.add('active-image');
-              this.removeActiveClass((imagesElements[i] as HTMLDivElement))
+              (imageElements[i] as HTMLDivElement).classList.add('zindex-image');
+              imageElements[i+1].classList.add('active-image');
+              dotElements[i+1].classList.add('active-dot');
+              this.removeActiveClass((imageElements[i] as HTMLDivElement));
+              this.removeDotActiveClass((dotElements[i] as HTMLDivElement));
               break;
             }
           }
@@ -40,13 +49,49 @@ export class BigSliderComponent implements OnInit {
       }, 4000);
     }, 100);
 
-
-
+    
+    (nextButton as HTMLDivElement).addEventListener('click', event => {
+      this.back(imageElements);
+      
+    });
   }
   removeActiveClass(element: HTMLDivElement){
     setTimeout(() => {
       element.classList.remove('active-image');
       element.classList.remove('zindex-image');
-    }, 1000);
+    }, 800);
+  }
+  removeDotActiveClass(dotElement: HTMLDivElement){
+    setTimeout(() => {
+      dotElement.classList.remove('active-dot');
+    }, 50);
+  }
+
+  back(imagesElements){
+    for(let i = 0; i < imagesElements.length; i++) {
+      if(imagesElements[i].classList.contains('active-image')){
+        if(i === 0){
+          imagesElements[imagesElements.length-1].classList.add('active-image');
+          this.removeActiveClass(imagesElements[i]);
+        }else {
+          imagesElements[i-1].classList.add('active-image');
+          this.removeActiveClass(imagesElements[i]);
+        }
+      }
+    }
+  }
+
+  next(imagesElements){
+    for(let i = 0; i < imagesElements.length; i++) {
+      if(imagesElements[i].classList.contains('active-image')){
+        if(i === imagesElements.length-1){
+          imagesElements[0].classList.add('active-image');
+          this.removeActiveClass(imagesElements[i]);
+        }else {
+          imagesElements[i+1].classList.add('active-image');
+          this.removeActiveClass(imagesElements[i]);
+        }
+      }
+    }
   }
 }
